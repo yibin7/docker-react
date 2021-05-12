76236cfc99a02dbc8cfc231ee005d3e9e760295d
yxin <yxin@a10networks.com>
2021-05-11 22:36:33 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Xin Say Edit <code>src/App.js</code> and save to reload.
+          Xin Say I was changed on the Feature branch  Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
